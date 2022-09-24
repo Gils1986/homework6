f94a7728e63a893473f0b6d481c4f1e0fae2a277
Gils1986 <gil.shterman@gmail.com>
2022-09-24 11:52:15 +0300

    homework6

---
+++ script.js
@@ -0,0 +1,27 @@
+// let newArray = ["1", "5", "87", "99", "114", "-6", "987", "65"];
+// let a = document.getElementById("demo");
+
+// function biggerThenUserNumber() {
+//   document.getElementById("Nemo").innerHTML = "";
+//   for (i = 0; i < newArray.length; i++) {
+//     if (a < newArray[i]) {
+//       document.getElementById("Nemo");
+//       innerHTML += newArray[i] + ",";
+//     }
+//   }
+// }
+
+// a.addEventListener("keyup", biggerThenUserNumber);
+
+// let numbers = [5, 8, 20, 1, 2, 16, 17, 3, 5];
+// let a = document.getElementById("koko");
+// function biggerThanUserNumber() {
+//   document.getElementById("demo").innerHTML = "";
+//   for (i = 0; i < numbers.length; i++) {
+//     if (a.value < numbers[i]) {
+//       document.getElementById("demo").innerHTML += numbers[i] + ",";
+//     }
+//   }
+// }
+
+// a.addEventListener("keyup", biggerThanUserNumber);
